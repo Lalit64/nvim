e933f88e076b5a05e2b69f542f74701e66a8f0d3
Lalit Yalamanchili <lalit.yalamanchili@gmail.com>
2025-04-29 12:14:42 +0530

    add undercurl to colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -11,6 +11,21 @@ return {
           blink_cmp = true,
           noice = true,
           nvimtree = true,
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+              errors = { "italic" },
+              hints = { "italic" },
+              warnings = { "italic" },
+              information = { "italic" },
+            },
+            underlines = {
+              errors = { "undercurl" },
+              hints = { "undercurl" },
+              warnings = { "undercurl" },
+              information = { "undercurl" },
+            },
+          },
           snacks = {
             enabled = true,
             indent_scope_color = "blue",
